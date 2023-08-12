d623008487eba8ace5fe2d39ca1bf62f57db92fd
Onur Öztaş <github@onuroztas.com>
2023-08-12 18:36:05 +0300

    Add color to console logs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const colors = require("colors");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -28,12 +29,15 @@ app.use("/api/v1/quotes", quotes);
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.bgGreen
+      .bold
+  )
 );
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // Close server & exit process
   server.close(() => process.exit(1));
 });
