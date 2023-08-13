beb43a364e5f60e0d203f11bb3124ae57d365754
Onur Öztaş <github@onuroztas.com>
2023-08-13 04:21:01 +0300

    Implement getQuotes, getQuote and updateQuote

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -1,17 +1,31 @@
-const Quote = require("../models/Quotes");
+const Quote = require("../models/Quote");
 
 // @desc        Get all quotes
 // @route       GET /api/v1/quotes
 // @access      Public
-exports.getQuotes = (req, res, next) => {
-  res.status(200).json({ succes: true, msg: "Show all quotes" });
+exports.getQuotes = async (req, res, next) => {
+  try {
+    const quote = await Quote.find();
+
+    res.status(200).json({ success: true, data: quote });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc        Get single quote
 // @route       GET /api/v1/quote/:id
 // @access      Public
-exports.getQuote = (req, res, next) => {
-  res.status(200).json({ succes: true, msg: `Show quote ${req.params.id}` });
+exports.getQuote = async (req, res, next) => {
+  try {
+    const quote = await Quote.findById(req.params.id);
+    if (!quote) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: quote });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc        Create new quote
@@ -25,7 +39,6 @@ exports.createQuote = async (req, res, next) => {
       data: quote,
     });
   } catch (err) {
-    console.log(err);
     res.status(400).json({
       success: false,
     });
@@ -35,13 +48,28 @@ exports.createQuote = async (req, res, next) => {
 // @desc        Update quote
 // @route       PUT /api/v1/quote/:id
 // @access      Private
-exports.updateQuote = (req, res, next) => {
-  res.status(200).json({ succes: true, msg: `Update quote ${req.params.id}` });
+exports.updateQuote = async (req, res, next) => {
+  try {
+    const quote = await Quote.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!quote) {
+      return res.status(400).json({ succes: false });
+    }
+
+    res.status(200).json({ success: true, data: quote });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 // @desc        Delete quote
 // @route       Delete /api/v1/quote/:id
 // @access      Private
 exports.deleteQuote = (req, res, next) => {
-  res.status(200).json({ succes: true, msg: `Delete quote ${req.params.id}` });
+  res.status(200).json({ success: true, msg: `Delete quote ${req.params.id}` });
 };
