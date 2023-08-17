8e42466112083e021c41f5adf18f4a39615b6d36
Onur Öztaş <github@onuroztas.com>
2023-08-18 02:19:40 +0300

    Handle duplicate fields

--- middleware/error.js
+++ middleware/error.js
@@ -11,6 +11,12 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 404);
   }
 
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || "Server Error",
