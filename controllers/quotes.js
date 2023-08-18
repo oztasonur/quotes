26c743abb0d1fd7aefc13d2d0a612f92071c3b76
Onur Öztaş <github@onuroztas.com>
2023-08-18 16:00:54 +0300

    Add async handler

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -1,10 +1,11 @@
 const Quote = require("../models/Quote");
 const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
 
 // @desc        Get all quotes
 // @route       GET /api/v1/quotes
 // @access      Public
-exports.getQuotes = async (req, res, next) => {
+exports.getQuotes = asyncHandler(async (req, res, next) => {
   //console.log(req.query);
   try {
     let query;
@@ -96,12 +97,12 @@ exports.getQuotes = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-};
+});
 
 // @desc        Get single quote
 // @route       GET /api/v1/quote/:id
 // @access      Public
-exports.getQuote = async (req, res, next) => {
+exports.getQuote = asyncHandler(async (req, res, next) => {
   try {
     const quote = await Quote.findById(req.params.id);
     if (!quote) {
@@ -113,12 +114,12 @@ exports.getQuote = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-};
+});
 
 // @desc        Create new quote
 // @route       POST /api/v1/quote
 // @access      Private
-exports.createQuote = async (req, res, next) => {
+exports.createQuote = asyncHandler(async (req, res, next) => {
   try {
     const quote = await Quote.create(req.body);
     res.status(201).json({
@@ -128,12 +129,12 @@ exports.createQuote = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-};
+});
 
 // @desc        Update quote
 // @route       PUT /api/v1/quote/:id
 // @access      Private
-exports.updateQuote = async (req, res, next) => {
+exports.updateQuote = asyncHandler(async (req, res, next) => {
   try {
     const quote = await Quote.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
@@ -150,12 +151,12 @@ exports.updateQuote = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-};
+});
 
 // @desc        Delete quote
 // @route       Delete /api/v1/quote/:id
 // @access      Private
-exports.deleteQuote = async (req, res, next) => {
+exports.deleteQuote = asyncHandler(async (req, res, next) => {
   try {
     const quote = await Quote.findByIdAndDelete(req.params.id);
 
@@ -169,4 +170,4 @@ exports.deleteQuote = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-};
+});
